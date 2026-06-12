404013095dedbbfde1b35ecca8b8b4646b6f539d
biswasuser <lohanibiswas1@gmail.com>
2026-06-12 22:56:06 +0545

    fectch dummyemployee data

--- client/src/pages/Employees.jsx
+++ client/src/pages/Employees.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { dummyEmployeeData } from '../assets/assets';
 
 const Employees = () => {
+
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchEmployees = useCallback(async () => {
+
+    setLoading(true);
+    setEmployees(dummyEmployeeData);
+    setTimeout(() => {
+        setLoading(false)
+    }, 1000)
+    
+  }, [])
+
+  useEffect(() => {
+
+    fetchEmployees();
+
+  },[])
+
   return (
     <div>Employees</div>
   )
